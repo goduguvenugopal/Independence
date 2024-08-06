97d41c0d3c8f0feb009af8524fd27cc209745c98
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-08-06 16:46:00 +0530

    added countdown modal

--- script.js
+++ script.js
@@ -151,101 +151,111 @@ closeBtn.addEventListener("click", () => {
 
 // freedomFighters Array list with names , images and about
 
- 
 const fighters = [
-  { 
-    name: 'Mahatma Gandhi', 
-    image: '/greatestFighters/image1.jpeg', 
-    about: 'Leader of the Indian independence movement and advocate of nonviolent resistance. He inspired movements for civil rights and freedom across the world. He is also known as the Father of the Nation in India for his efforts in leading the country to independence through peaceful means.'
+  {
+    name: "Mahatma Gandhi",
+    image: "/greatestFighters/image1.jpeg",
+    about:
+      "Leader of the Indian independence movement and advocate of nonviolent resistance. He inspired movements for civil rights and freedom across the world. He is also known as the Father of the Nation in India for his efforts in leading the country to independence through peaceful means.",
   },
-  { 
-    name: 'Bal Gangadhar Tilak', 
-    image: '/greatestFighters/image2.jpeg', 
-    about: 'Indian nationalist, teacher, and an independence activist. He was one of the first leaders of the Indian Independence Movement and was often called "The father of Indian unrest". His famous declaration, "Swaraj is my birthright, and I shall have it," inspired many future leaders.'
+  {
+    name: "Bal Gangadhar Tilak",
+    image: "/greatestFighters/image2.jpeg",
+    about:
+      'Indian nationalist, teacher, and an independence activist. He was one of the first leaders of the Indian Independence Movement and was often called "The father of Indian unrest". His famous declaration, "Swaraj is my birthright, and I shall have it," inspired many future leaders.',
   },
-  { 
-    name: 'Subhas Chandra Bose', 
-    image: '/greatestFighters/image3.jpeg', 
-    about: 'Indian nationalist whose defiant patriotism made him a hero in India. He was a prominent figure in the Indian independence movement and led the Indian National Army against British forces during World War II. His legacy continues to inspire Indians today.'
+  {
+    name: "Subhas Chandra Bose",
+    image: "/greatestFighters/image3.jpeg",
+    about:
+      "Indian nationalist whose defiant patriotism made him a hero in India. He was a prominent figure in the Indian independence movement and led the Indian National Army against British forces during World War II. His legacy continues to inspire Indians today.",
   },
-  { 
-    name: 'Bhagat Singh', 
-    image: '/greatestFighters/image4.jpeg', 
-    about: 'Indian socialist revolutionary whose acts of dramatic violence against the British government helped inspire the Indian independence movement. He is considered a hero by many Indians for his daring and commitment to the cause of freedom. His execution turned him into a martyr.'
+  {
+    name: "Bhagat Singh",
+    image: "/greatestFighters/image4.jpeg",
+    about:
+      "Indian socialist revolutionary whose acts of dramatic violence against the British government helped inspire the Indian independence movement. He is considered a hero by many Indians for his daring and commitment to the cause of freedom. His execution turned him into a martyr.",
   },
-  { 
-    name: 'Sardar Vallabhbhai Patel', 
-    image: '/greatestFighters/image5.jpeg', 
-    about: 'First Deputy Prime Minister and Home Minister of India, known for his role in the integration of the princely states into the Indian Union. He is remembered as the "Iron Man of India" for his strong leadership and efforts to unify the country after independence.'
+  {
+    name: "Sardar Vallabhbhai Patel",
+    image: "/greatestFighters/image5.jpeg",
+    about:
+      'First Deputy Prime Minister and Home Minister of India, known for his role in the integration of the princely states into the Indian Union. He is remembered as the "Iron Man of India" for his strong leadership and efforts to unify the country after independence.',
   },
-  { 
-    name: 'Jawaharlal Nehru', 
-    image: '/greatestFighters/image6.jpeg', 
-    about: 'First Prime Minister of India and a central figure in Indian politics both before and after independence. He was a leading figure in the Indian independence movement and worked closely with Mahatma Gandhi. His policies shaped modern India.'
+  {
+    name: "Jawaharlal Nehru",
+    image: "/greatestFighters/image6.jpeg",
+    about:
+      "First Prime Minister of India and a central figure in Indian politics both before and after independence. He was a leading figure in the Indian independence movement and worked closely with Mahatma Gandhi. His policies shaped modern India.",
   },
-  { 
-    name: 'Chandra Shekhar Azad', 
-    image: '/greatestFighters/image7.jpeg', 
-    about: 'Indian revolutionary who reorganized the Hindustan Republican Association under its new name of Hindustan Socialist Republican Association after the death of its founder, Ramprasad Bismil. He vowed never to be captured alive by the British.'
+  {
+    name: "Chandra Shekhar Azad",
+    image: "/greatestFighters/image7.jpeg",
+    about:
+      "Indian revolutionary who reorganized the Hindustan Republican Association under its new name of Hindustan Socialist Republican Association after the death of its founder, Ramprasad Bismil. He vowed never to be captured alive by the British.",
   },
-  { 
-    name: 'Lal Bahadur Shastri', 
-    image: '/greatestFighters/image8.jpeg', 
-    about: 'Second Prime Minister of India and a senior leader of the Indian National Congress. He promoted the White Revolution – a national campaign to increase the production and supply of milk – and the Green Revolution to boost India’s food production.'
+  {
+    name: "Lal Bahadur Shastri",
+    image: "/greatestFighters/image8.jpeg",
+    about:
+      "Second Prime Minister of India and a senior leader of the Indian National Congress. He promoted the White Revolution – a national campaign to increase the production and supply of milk – and the Green Revolution to boost India’s food production.",
   },
-  { 
-    name: 'Dr Rajendra Prasad', 
-    image: '/greatestFighters/image9.jpeg', 
-    about: 'First President of India, serving from 1950 to 1962. He was a leader of the Indian independence movement and played a crucial role in the drafting of the Indian Constitution. He is remembered for his humility and dedication to public service.'
+  {
+    name: "Dr Rajendra Prasad",
+    image: "/greatestFighters/image9.jpeg",
+    about:
+      "First President of India, serving from 1950 to 1962. He was a leader of the Indian independence movement and played a crucial role in the drafting of the Indian Constitution. He is remembered for his humility and dedication to public service.",
   },
-  { 
-    name: 'Gopal Krishna Gokhale', 
-    image: '/greatestFighters/image10.jpeg', 
-    about: 'Indian political leader, social reformer, and a senior leader of the Indian National Congress. He was a mentor to Mahatma Gandhi and advocated for social and political reforms through moderate means. He founded the Servants of India Society.'
+  {
+    name: "Gopal Krishna Gokhale",
+    image: "/greatestFighters/image10.jpeg",
+    about:
+      "Indian political leader, social reformer, and a senior leader of the Indian National Congress. He was a mentor to Mahatma Gandhi and advocated for social and political reforms through moderate means. He founded the Servants of India Society.",
   },
-  { 
+  {
     name: "Dr. Br. Ambedkar",
     image: "/greatestFighters/image11.jpeg",
     about:
-      " Dr. B.R. Ambedkar is a famous freedom fighter in India.His sacrifices had stable social ostracism and faced opposition from traditional caste hierarchies. Also, Ambedkar’s legacy continues to inspire millions of people with social justice and a symbol of hope for marginalised communities in India.",
+      " Dr. B.R. Ambedkar is a famous freedom fighter in India.His sacrifices had stable social ostracism and faced opposition from traditional caste hierarchies and Ambedkar’s legacy continues to inspire millions of people.",
   },
-  { 
-    name: 'Dadabhai Naoroji', 
-    image: '/greatestFighters/image12.jpeg', 
-    about: 'Indian political leader, educator, and an early Indian political and social leader. He was the first Asian to be a British MP and worked tirelessly for the cause of Indian self-rule. He is known as the "Grand Old Man of India."'
+  {
+    name: "Dadabhai Naoroji",
+    image: "/greatestFighters/image12.jpeg",
+    about:
+      'Indian political leader, educator, and an early Indian political and social leader. He was the first Asian to be a British MP and worked tirelessly for the cause of Indian self-rule. He is known as the "Grand Old Man of India."',
   },
-  { 
-    name: 'Mangal Pandey', 
-    image: '/greatestFighters/image13.jpeg', 
-    about: 'Indian soldier who played a key part in events immediately preceding the outbreak of the Indian rebellion of 1857. He is often considered the first martyr in India’s struggle for independence from British rule.'
+  {
+    name: "Mangal Pandey",
+    image: "/greatestFighters/image13.jpeg",
+    about:
+      "Indian soldier who played a key part in events immediately preceding the outbreak of the Indian rebellion of 1857. He is often considered the first martyr in India’s struggle for independence from British rule.",
   },
-  { 
+  {
     name: "Alluri Sitarama Raju",
     image: "/greatestFighters/image14.jpeg",
     about:
       " Alluri Sitarama Raju was an Indian revolutionary who fought against the British colonial rule in India. He led a guerrilla campaign in the Eastern Ghats region of present-day Andhra Pradesh, mobilizing the tribal people against the oppressive forest laws and policies of the British government",
-   },
-  { 
-    name: 'Rani Lakshmi Bai', 
-    image: '/greatestFighters/image15.jpeg', 
-    about: 'Queen of the Maratha-ruled princely state of Jhansi and a leader of the Indian Mutiny of 1857. She is remembered for her bravery and leadership in the fight against British rule and remains a symbol of resistance.'
   },
-  { 
-    name: 'Sarojini Naidu', 
-    image: '/greatestFighters/image16.jpeg', 
-    about: 'Indian independence activist, poet, and politician. She was the first woman to become the president of the Indian National Congress and the first woman to serve as a governor in independent India. Her literary work earned her the title "Nightingale of India."'
-  }
+  {
+    name: "Rani Lakshmi Bai",
+    image: "/greatestFighters/image15.jpeg",
+    about:
+      "Queen of the Maratha-ruled princely state of Jhansi and a leader of the Indian Mutiny of 1857. She is remembered for her bravery and leadership in the fight against British rule and remains a symbol of resistance.",
+  },
+  {
+    name: "Sarojini Naidu",
+    image: "/greatestFighters/image16.jpeg",
+    about:
+      'Indian independence activist, poet, and politician. She was the first woman to become the president of the Indian National Congress and the first woman to serve as a governor in independent India. Her literary work earned her the title "Nightingale of India."',
+  },
 ];
-
 
 const indiFightersCard = document.getElementById("indi-fighters-Card");
 
- 
-
 window.addEventListener("load", () => {
-  let htmlContent = '';
-  fighters.forEach((item) => {
-    htmlContent += `<div class="col col-md-6 col-lg-4 col-xl-3">
+  let htmlContent = "";
+  fighters.map((item) => {
+    indiFightersCard.innerHTML += `<div class="col col-md-6 col-lg-4 col-xl-3">
         <div class="card shadow" style="width: 18.5rem;" id="card-for-f">
           <img src=${item.image} class="card-img-top" alt="${item.name}">
           <div class="card-body">
@@ -255,8 +265,6 @@ window.addEventListener("load", () => {
         </div>
       </div>`;
   });
-
-  indiFightersCard.innerHTML = htmlContent;
 
   console.log(htmlContent);
 });
