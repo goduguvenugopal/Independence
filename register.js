428e5fc8597a3c7134fd501ae02171264862e477
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-08-08 13:16:11 +0530

    added form submitting api

--- register.js
+++ register.js
@@ -435,6 +435,8 @@ const occupation = document.getElementById("occupation-box");
 const stateBox = document.getElementById("state-box");
 const cityBox = document.getElementById("city-box");
 const searchCityBox = document.getElementById("search-city-box");
+const fullName = document.getElementById("full-name-box");
+const email = document.getElementById("email-box");
 
 window.addEventListener("load", () => {
   // Adding the occupation options
@@ -448,10 +450,12 @@ window.addEventListener("load", () => {
   });
 });
 
-searchCityBox.addEventListener("click" , ()=>{
+searchCityBox.addEventListener("click", () => {
   cityBox.style.display = "block";
-})
- 
+  allDistricts.map((item) => {
+    cityBox.innerHTML += `<h6 class="district-name" onclick="city('${item}')">${item}</h6>`;
+  });
+});
 
 searchCityBox.addEventListener("change", () => {
   cityBox.style.display = "block";
@@ -472,17 +476,109 @@ searchCityBox.addEventListener("change", () => {
   }
 });
 
-function city(city){
-  searchCityBox.value = city
+function city(city) {
+  searchCityBox.value = city;
   cityBox.style.display = "none";
 }
 
-formFunc.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const text = searchCityBox.value.trim()
-  if(text === ""){
-    alert("Please fill the all fields")
-  }else if(text !== ""){
-    alert("registration completed successfully")
+const nameValidation = document.getElementById("name-validation");
+const emailValidation = document.getElementById("email-validation");
+const occuValidation = document.getElementById("occupation-validation");
+const stateValidation = document.getElementById("state-validation");
+const cityValidation = document.getElementById("city-validation");
+const pinBox = document.getElementById("pin-box");
+const valid_Email = document.getElementById("valid-email");
+const formSuccPop = document.getElementById("form-pop-up");
+document.getElementById("close-count-bt").addEventListener("click", () => {
+  formSuccPop.style.display = "none";
+});
+const spin_sub_bt = document.getElementById("spin-sub-bt");
+
+formFunc.addEventListener("click", async () => {
+  const fullName1 = fullName.value.trim();
+  const email1 = email.value.trim();
+  const state = stateBox.value.trim();
+  const occupation1 = occupation.value.trim();
+  const city1 = searchCityBox.value.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  // form validation logic
+  if (fullName1 === "") {
+    nameValidation.style.display = "block";
+  } else if (email1 === "") {
+    emailValidation.style.display = "block";
+  } else if (occupation1 === "") {
+    occuValidation.style.display = "block";
+  } else if (state === "") {
+    stateValidation.style.display = "block";
+  } else if (city1 === "") {
+    cityValidation.style.display = "block";
+  } else if (!emailRegex.test(email1)) {
+    valid_Email.style.display = "block";
+  } else {
+    formFunc.style.display = "none";
+    spin_sub_bt.style.display = "block";
+
+    const image =
+      "https://media.istockphoto.com/id/2165184709/vector/78th-indian-independence-day-15th-august-social-media-post-poster-banner-web-banner-print.jpg?s=612x612&w=0&k=20&c=a2OQbBwD4eBj1K3VQiJ9Jw45TrNAoRcwjVBXeEQiaqk=";
+
+    //sending email registration details
+    const html = `
+<h3> Dear ${fullName1},</h3>
+<h3 class="mt-3"> We are thrilled to inform you that your registration for our Independence Event has been successfully completed!</h3>
+<img src=${image} alt="independence" style="max-width:100%; height:200px;" class="mt-3 "/>
+   <h3 class="my-1">Event details : </h3>
+<h4 class="my-1">Event : Independence Day Celebration</h4>
+  <h4 class="my-1">Date : 15 August 2024 </h4>
+  <h4 class="my-1">Venue : Red Fort Delhi India</h4>
+<h4 class="mt-3">Please find your token number and detailed information attached to this email.
+Kindly print a hard copy of this email and bring it with you to the event. This will serve as your entry pass. </h4>
+<h4 class="my-3">Token Number : 123456</h4>
+<h4>We look forward to celebrating this special occasion with you!</h4>
+<h5 class="text-secondary my-3">If you have any questions or need further assistance, please do not hesitate to contact us.</h5>
+<button style="height:35px; width:130px;" class="btn bg-primary text-white"> <a class="text-white" href="https://independencei.netlify.app/" target="_blank">Contact us</a></button>
+ `;
+
+    // mail properties
+    const formData = {
+      to: email1,
+      subject: "Independence Event Registration Confirmation",
+      html: html,
+    };
+
+    try {
+      // sending mail to the registration details
+      const response = await fetch(
+        "https://fashionkart-server.onrender.com/mail/sendmail",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (response) {
+        nameValidation.style.display = "none";
+        emailValidation.style.display = "none";
+        occuValidation.style.display = "none";
+        stateValidation.style.display = "none";
+        valid_Email.style.display = "none";
+        cityValidation.style.display = "none";
+        fullName.value = "";
+        email.value = "";
+        stateBox.value = "";
+        occupation.value = "";
+        searchCityBox.value = "";
+        pinBox.value = "";
+        formFunc.style.display = "block";
+        spin_sub_bt.style.display = "none";
+        formSuccPop.style.display = "block";
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Please try again registration has failed");
+    }
   }
 });
